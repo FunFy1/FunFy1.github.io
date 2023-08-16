7dd013b9faf78d4cf16e96e939f29d077ffee882
FunFy1 <wzp3106@126.com>
2023-08-16 14:57:57 +0800

    Site updated: 2023-08-16 14:57:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/06/14/加入我们/","2023/06/16/使用雨云搭建BC端群组服/","2023/05/19/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/06/14/加入我们/","2023/08/16/白嫖MC服务器搭建小游戏服务器！/","2023/06/16/使用雨云搭建BC端群组服/","2023/05/19/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
